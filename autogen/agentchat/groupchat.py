871b99b14c4a11efb06d4f4b8d6c85492957b36e
jagdeep sidhu <sidhujag@syscoin.org>
2023-11-09 13:22:01 -0800

    add new def field decl's

--- autogen/agentchat/groupchat.py
+++ autogen/agentchat/groupchat.py
@@ -120,6 +120,11 @@ class GroupChatManager(ConversableAgent):
         # Allow async chat if initiated using a_initiate_chat
         self.register_reply(Agent, GroupChatManager.a_run_chat, config=groupchat, reset_config=GroupChat.reset)
         self.groupchat = groupchat
+        self.auth = None
+        self.description = ""
+        self.delegator = None
+        self.incoming = {}
+        self.outgoing = {}
         # self._random = random.Random(seed)
 
     def run_chat(
